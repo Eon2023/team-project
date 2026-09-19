efd001d99a7809dc7801576dddc768b9f67a6df9
Karelashvili T.I. <reaper_februar2023@mail.ru>
2026-09-19 15:43:36 +0700

    feat: перенесены функции варианта 56, добавлены пункты меню 1-3

---
+++ techlid.h
@@ -0,0 +1,9 @@
+#ifndef TECHLID_H
+#define TECHLID_H
+
+// Функции варианта 56 (Карелашвили Т.И.)
+double absValue(double x);
+int sign(double x);
+double clamp(double x, double lo, double hi);
+
+#endif // TECHLID.H
